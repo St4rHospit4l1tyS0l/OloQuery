dd4e933393d42176c4c1ce63968a10f8cd8b1579
Israel Vite Silva <israelvi@hotmail.com>
2015-12-29 19:22:45 -0600

    orden lista

---
+++ AlohaOrderOnLine/Service/Menus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AlohaOrderOnLine.Infrastructure;
+
+namespace AlohaOrderOnLine.Service
+{
+    public class Menus
+    {
+        public string GetMenuInfo(string uri, string menuId)
+        {
+            Console.WriteLine(string.Format("{0} ", uri + "Menus/" + menuId));
+            return GetMethod.GetRequest(uri + "Menus/" + menuId);
+        }
+    }
+}
